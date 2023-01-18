2bd2f5b52171f9e997da2f225e49754bef3b66bf
Mrk0p <sachinwele23@gmail.com>
2023-01-18 17:29:54 +0530

    Added : Full Time Wage logic

--- src/com/bridgelabz/employee_wage/EmployeeWage.java
+++ src/com/bridgelabz/employee_wage/EmployeeWage.java
@@ -6,16 +6,23 @@ public class EmployeeWage {
 
     public static void main(String[] args) {
 
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int dailyWage = 0;
+
         System.out.println("Welcome to Employee Wage Computation Program");
 
         Random random = new Random();
 
         int randomNum = random.nextInt(2);
 
-        if (randomNum == 1)
+        if (randomNum == 1) {
             System.out.println("Employee  is Present");
+            dailyWage = wagePerHour * fullDayHours;
+        }
         else
             System.out.println("Employee  is Absent");
+        System.out.println("daily wage is :"+ dailyWage);
 
     }
 
